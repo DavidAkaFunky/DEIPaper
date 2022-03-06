3dd0072a33a70e23eacb18dd64ea919d9559f931
FunkyCracky <futebol4@hotmail.com>
2022-03-06 16:18:31 +0000

    More progress
    .

--- src/DEIPaperApp/papers.py
+++ src/DEIPaperApp/papers.py
@@ -1,12 +1,16 @@
-from django.shortcuts import redirect
-from django.http import HttpRequest, HttpResponse
+from django.contrib import messages
+from django.http import HttpRequest, HttpResponse, HttpResponseRedirect
 from json import JSONDecodeError
-from . import views
 import requests
 
-API_URL = "https://aduck.rnl.tecnico.ulisboa.pt/istpaper/papers"
+API_URL = "https://aduck.rnl.tecnico.ulisboa.pt/istpaper/papers/"
 API_AUTH = "Bearer ist195550" # This should be hidden in production, for obvious reasons
 
+GET_PAPERS_FAIL = "There was a problem fetching the papers list."
+GET_PAPER_FAIL = "There was a problem fetching the requested paper."
+POST_PAPER_FAIL = "There was a problem adding the new paper to the system."
+DEL_PAPER_FAIL = "There was a problem removing the paper from the system."
+UPD_PAPER_FAIL = "There was a problem updating the paper in the system."
 INV_LIM = "Invalid limit/offset"
 INV_ACC_TOKEN = "Access token missing or invalid"
 INV_PAPER = "Invalid paper"
@@ -25,7 +29,18 @@ def get_papers(offset: int, lines: int) -> dict:
             return r.json()
         except JSONDecodeError:
             pass
-    raise DEIPaperError(r.code, msg_dict = errors)
+    raise DEIPaperError(r.status_code, GET_PAPERS_FAIL, msg_dict = errors)
+
+def get_paper(paper_id) -> dict:
+    errors = {400: INV_PAPER, 404: INV_PAPER_ID}
+    r = requests.get(API_URL,
+                     params = {"paperId": paper_id})
+    if r.ok:
+        try:
+            return r.json()
+        except JSONDecodeError:
+            pass
+    raise DEIPaperError(r.status_code, GET_PAPER_FAIL, msg_dict = errors)
 
 def post_paper(paper) -> int:
     errors = {400: INV_PAPER, 401: INV_ACC_TOKEN}
@@ -34,25 +49,37 @@ def post_paper(paper) -> int:
                       json = paper)
     if r.ok:
         try:
-            return r.json()["id"] # Return the newly created paper"s id
+            return r.json()["id"] # Return the newly created paper's id
         except JSONDecodeError:
             pass
-    raise DEIPaperError(r.code, msg_dict = errors)
+    raise DEIPaperError(r.status_code, POST_PAPER_FAIL, msg_dict = errors)
 
 def delete_paper(paper_id) -> None:
     errors = {400: INV_PAPER_ID, 401: INV_ACC_TOKEN, 403: NO_PERM, 404: PAPER_NOT_FOUND}
-    r = requests.delete(API_URL + str(paper_id),
+    r = requests.delete(API_URL,
+                        params = {"paperId": paper_id},
                         headers = {"Authorization": API_AUTH})
     if not r.ok:
-        raise DEIPaperError(r.code, msg_dict = errors)
+        raise DEIPaperError(r.status_code, DEL_PAPER_FAIL, msg_dict = errors)
+
+def update_paper(paper_id) -> None:
+    errors = {400: INV_PAPER, 403: NO_PERM, 404: PAPER_NOT_FOUND}
+    r = requests.put(API_URL,
+                     params = {"paperId": paper_id},
+                     headers = {"Authorization": API_AUTH})
+    if not r.ok:
+        raise DEIPaperError(r.status_code, UPD_PAPER_FAIL, msg_dict = errors)
 
 class DEIPaperError(Exception):
-    def __init__ (self, code: int, msg_dict: dict) -> None:
+    def __init__ (self, code: int, title: str, msg_dict: dict) -> None:
         self.code = code
+        self.title = title
         try:
             self.msg = msg_dict[code]
         except KeyError:
             self.msg = UNEXPECTED_ERROR
 
-    def render_error(self, request: HttpRequest, self_redirect = False) -> HttpResponse:
-        return redirect(request, views.main_page, self_redirect)
+    def show_error(self, request: HttpRequest, redirect_flag = True) -> HttpResponse:
+        messages.error(request, f"{self.title} Error {self.code}: {self.msg}")
+        if redirect_flag:
+            return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
--- src/DEIPaperApp/views.py
+++ src/DEIPaperApp/views.py
@@ -1,19 +1,9 @@
 from django.shortcuts import render, redirect
+from django.contrib import messages
 from django.http import HttpRequest, HttpResponse, HttpResponseRedirect
 from . import papers, forms
 
-def main_page(request: HttpRequest, page = 1, lines = 10, self_redirect = False, err_code = None, err_msg = None):
-    """Auxiliary function to stop the main page from
-    redirecting to itself if it fails loading"""
-    if self_redirect:
-        return render(request,
-                      "DEIPaperApp/list.html",
-                      {"error": True,
-                       "code": err_code,
-                       "msg": err_msg})
-    return list_papers(request, page, lines)
-
-def list_papers(request: HttpRequest, page: int, lines: int) -> HttpResponse:
+def list_papers(request: HttpRequest, page = 1, lines = 10) -> HttpResponse:
     """Show a page with a list containing a
     maximum of the given amount of papers"""
     try:
@@ -31,7 +21,7 @@ def list_papers(request: HttpRequest, page: int, lines: int) -> HttpResponse:
                        "has_prev_page": has_prev_page,
                        "size_options": (5, 10, 20, 50)})
     except papers.DEIPaperError as e:
-        return e.render_error(request, self_redirect = True)
+        return e.show_error(request, redirect_flag = False)
 
 def new_paper(request: HttpRequest) -> HttpResponse:
     """Show a page allowing an authenticated user
@@ -41,9 +31,10 @@ def new_paper(request: HttpRequest) -> HttpResponse:
         if form.is_valid():
             try:
                 paper_id = papers.post_paper(form.create_json())
-                return redirect("/", {"add_success": True}) # TO-DO: paper_id needs to be added
+                messages.success(request, f"Paper added successfully. Paper ID: {paper_id}")
+                return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
             except papers.DEIPaperError as e:
-                return e.render_error(request)
+                return e.show_error(request)
     else:
         form = forms.NewPaperForm()
 
@@ -58,6 +49,7 @@ def update_paper(request, paper_id):
 def delete_paper(request, paper_id):
     try:
         papers.delete_paper(paper_id)
-        return redirect("/", {"del_success": True})
+        messages.success(request, f"Paper with ID {paper_id} deleted successfully.")
+        return redirect("/")
     except papers.DEIPaperError as e:
-        return e.render_error(request)
+        return e.show_error(request)
